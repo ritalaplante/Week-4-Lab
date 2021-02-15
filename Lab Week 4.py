0d48873ac2834612e137af467ec0c138a17b9492
ritalaplante <45489428+ritalaplante@users.noreply.github.com>
2021-02-15 12:21:16 -0500

    Add files via upload

---
+++ Lab Week 4.py
@@ -0,0 +1,18 @@
+#Rita LaPlante
+#Lab Week 4
+
+#Part 1: For loops
+#1. Print out every number between 10 and 100
+#2. Print out every other number between 10 and 50
+#3. Do a countdown backwards from 100 to 50
+#4. Do a countdown backwards from 100 by 7's
+
+#Part 2: And, or, if, while
+#You are giving out prizes to people based on their age, name, and number of cats.
+#People over 18 whose names begin with "E" and people between 25 and 50 whose
+#names contain an "a" win a new car. People with more than 5 cats who are over 60
+#get a new house
+
+#Part 3: Modules
+#1. Print out a random number using random()
+#2. Use the re module with re.sub to replace a character in a word
