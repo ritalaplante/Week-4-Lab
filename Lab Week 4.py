c17b19c9403f2ea698d8fbe982b21e238d86548d
ritalaplante <45489428+ritalaplante@users.noreply.github.com>
2021-02-15 12:23:42 -0500

    Add files via upload

---
+++ Lab Week 4.py
@@ -0,0 +1,32 @@
+#Rita LaPlante
+#Lab Week 4
+
+#Part 1: For loops
+#1. Print out every number between 10 and 100
+#2. Print out every other number between 10 and 50
+#3. Do a countdown backwards from 100 to 50
+#4. Do a countdown backwards from 100 by 7's
+
+#Part 2: And, or, if, while
+#You are giving out prizes to people based on their age, name, and number of cats.
+#People over 18 whose names begin with "E" and people between 25 and 50 whose
+#names contain an "a" win a new car. People with more than 5 cats who are over 60
+#get a new house
+
+#Nested if statements: Check if a year is a leap year. A leap year is divisible by 4 except
+#for century years. The century year is a leap year only if it is perfectly divisible by 400
+#Examples:
+#   2017 is not a leap year
+#   1900 is not a leap year
+#   2012 is a leap year
+#   2000 is a leap year
+
+#You want to make the perfect up of tea. In order to this you want to make sure
+#your tea is not too hot and your milk is not too cold. While the temperature
+#of the tea is above 180 degrees and the milk is below 40 degrees print "Tea
+#is not ready". Once the temperature of the milk and tea are correct, print
+#"Tea is just right"
+
+#Part 3: Modules
+#1. Print out a random number using random()
+#2. Use the re module with re.sub to replace a character in a word
